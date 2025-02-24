eea05b1e85548499eae3178ad4880b1f81449e6f
KrisVerses <krisincode@gmail.com>
2025-02-24 12:36:29 -0600

    styles: add custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Archivo Black", "serif"],
+        body: ["Inter", "sans-serif"],
+      },
+      colors: {
+        background: "#FCEC63",
+        content: "#222222",
+      },
+    },
   },
   plugins: [],
 };

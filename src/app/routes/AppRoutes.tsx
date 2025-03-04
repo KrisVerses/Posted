b2c95b8a4c142c16daff1f26a2d8d8f4f3d6a784
KrisVerses <krisincode@gmail.com>
2025-03-04 09:30:25 -0600

    chore: remove unused about page

--- src/app/routes/AppRoutes.tsx
+++ src/app/routes/AppRoutes.tsx
@@ -6,13 +6,11 @@ import {
 import { Home } from "../../pages/Home";
 import { Layout } from "../../components/Layout";
 import React from "react";
-import { About } from "../../pages/About";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="about" element={<About />} />
     </Route>
   )
 );
